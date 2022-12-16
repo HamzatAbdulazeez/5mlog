d8ef1a6c0870144401d02b974aca3207f2c4fbe6
princeemeka965 <anyanwue4@gmail.com>
2022-12-16 13:41:26 -0800

    made changes

--- src/services/auth.service.js
+++ src/services/auth.service.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = `${process.env.REACT_APP_BASE_URL}`;
+const API_URL = `https://5million.greenmouseproperties.com/api`;
 
 const register = (payload) => {
     return axios.post(API_URL + "/auth/register", payload);
