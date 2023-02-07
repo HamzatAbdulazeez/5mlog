2e84fb2f9c2a6813822ff3b66005dcd764f5a41a
Willscode <godswillijezei@gmail.com>
2023-02-07 16:09:48 +0100

    submit status

--- src/components/dashboard/pages/Freight/Form.js
+++ src/components/dashboard/pages/Freight/Form.js
@@ -1,4 +1,3 @@
-import { Input, Option, Select, Textarea } from '@material-tailwind/react'
 import React from 'react'
 import { useSelector} from 'react-redux';
 import { FreightAdmin } from './AdminForm';
--- src/components/dashboard/pages/Pickup/InterstateForm.js
+++ src/components/dashboard/pages/Pickup/InterstateForm.js
@@ -1,4 +1,3 @@
-import { Input, Textarea } from '@material-tailwind/react'
 import React from 'react'
 import { useSelector} from 'react-redux';
 import { InterStateAdmin } from './InterAdmin';
